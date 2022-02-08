727468ef69951c179d8de29075460e727cb5e2e0
Kevin-Emmanuel-Arouje <78081557+Kevin-Emmanuel-Arouje@users.noreply.github.com>
2022-02-08 15:56:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();    
+}
+function draw(){
+
+}
+function preload(){
+
+}
+function takeSnapshot(){
+    save("Image.jpg");
+}
